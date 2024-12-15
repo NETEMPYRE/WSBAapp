e3af12251061d47152afdba3a15f0e78b8273cdf
shayan <shayanofficeal>
2024-12-15 05:20:39 -0800

    admin dashboard

--- Requests/admin.py
+++ Requests/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Request
 # Register your models here.
+@admin.register(Request)
+class RequestAdmin(admin.ModelAdmin):
+    list_display = ('id', 'DateTime', 'PlayerID', 'AccountTitle', 'AccountNumber', 'PaymentMethod', 'Amount', 'Status')
+    search_fields = ('PlayerID', 'AccountTitle', 'PaymentMethod')
+    list_filter = ('Status', 'PaymentMethod')
--- WSBAapp/settings.py
+++ WSBAapp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6dq96_8=@7#jryf9ihmmvp)l!8_$x83a)!2^nq&%g=2llijjjp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
